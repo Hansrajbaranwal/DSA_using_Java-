a599b7d6ccf8f01d27e747bff6db13951277e8d1
Hansrajbaranwal <hrajbaranwal@gmail.com>
2023-09-06 23:02:28 +0530

    added 1st pattern code

---
+++ Pattern1.java
@@ -0,0 +1,10 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
